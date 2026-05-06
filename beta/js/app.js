34981a3eda45241c461697b3194637c9efcc173c
Admin <admin@local>
2026-05-06 04:59:25 -0500

    FINAL v12.4.0: Fixed syntax and giant trend indicators

--- beta/js/app.js
+++ beta/js/app.js
@@ -59,7 +59,7 @@ class App {
 
   async navigate() {
     const user = getSession();
-    const versionStr = "v12.4.0-FORCE";
+    const versionStr = "v12.4.0-FINAL";
     
     // [SEGURIDAD] Reiniciar contador de inactividad al navegar/entrar
     if (user) {
