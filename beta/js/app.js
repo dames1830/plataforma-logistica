3bbff2acd004e49bb54856988b86af1c2f963dff
Admin <admin@local>
2026-05-06 07:31:01 -0500

    FINAL v12.4.0: Fixed syntax and giant trend indicators

--- beta/js/app.js
+++ beta/js/app.js
@@ -59,7 +59,7 @@ class App {
 
   async navigate() {
     const user = getSession();
-    const versionStr = "v12.5.8-FINAL";
+    const versionStr = "v12.5.9-FINAL";
     
     // [SEGURIDAD] Reiniciar contador de inactividad al navegar/entrar
     if (user) {
--- beta/js/services/csvHub_v6.js
+++ beta/js/services/csvHub_v6.js
@@ -1041,7 +1041,7 @@ export const calculateBufferPallets = (configOverride = null) => {
 
 
     return { 
-        version: 'v12.5.8-FINAL',
+        version: 'v12.5.9-FINAL',
         totalReserva: globalRQ,
         detalle: detalleExplosionado, 
         detalleZonas, 
