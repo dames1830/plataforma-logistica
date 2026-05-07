1813ac677fa36966c131b87f4ffadb962ee3638a
Admin <admin@local>
2026-05-07 05:56:50 -0500

    BETA: Fixed syntax error in Tareas module v12.4.9-BETA

--- beta/js/views/dashboard_v6.js
+++ beta/js/views/dashboard_v6.js
@@ -1,8 +1,8 @@
-import { parseFile, parseBufferFiles, getAreaData, clearAreaData, generateKPIs, calculateBufferPallets, fetchBufferConfig, logSystemAction, pingServer, saveBufferReport, loadBufferReport, fetchBufferHistory, dataStore, setDateFilter, currentDateFilter, getUploadMeta, initPersistentData, updateTablaTallas } from '../services/csvHub_v6.js?v=12.4.8-BETA';
+import { parseFile, parseBufferFiles, getAreaData, clearAreaData, generateKPIs, calculateBufferPallets, fetchBufferConfig, logSystemAction, pingServer, saveBufferReport, loadBufferReport, fetchBufferHistory, dataStore, setDateFilter, currentDateFilter, getUploadMeta, initPersistentData, updateTablaTallas } from '../services/csvHub_v6.js?v=12.4.9-BETA';
 import * as adminService from '../services/adminService.js?v=12.1.86-BETA';
 
 
-const VERSION = '12.4.8-BETA';
+const VERSION = '12.4.9-BETA';
 const CACHE_KEY = `logistics_v12_3_0_`;
 console.log(`[PULSE] Engine v${VERSION} Initialized (Beta / Cache Force)`);
 
@@ -2760,7 +2760,6 @@ export const renderDashboard = async (container, user, onLogout) => {
         almacenajeTaskMode = mode;
         renderAlmacenajeTareas(container);
     };
-  };
   };
 
   renderNav();
