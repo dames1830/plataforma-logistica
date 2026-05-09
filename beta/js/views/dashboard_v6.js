ef1130dd8a8e7bebaeec3fb53a7f0886575e8995
Admin <admin@local>
2026-05-08 19:44:45 -0500

    Forced synchronization of dependency versions in v12.4.90-BETA

--- beta/js/views/dashboard_v6.js
+++ beta/js/views/dashboard_v6.js
@@ -1,9 +1,9 @@
-import { parseFile, parseBufferFiles, getAreaData, clearAreaData, generateKPIs, calculateBufferPallets, fetchBufferConfig, logSystemAction, pingServer, saveBufferReport, loadBufferReport, fetchBufferHistory, dataStore, setDateFilter, currentDateFilter, getUploadMeta, initPersistentData, updateTablaTallas } from '../services/csvHub_v6.js?v=12.4.88';
-import * as adminService from '../services/adminService.js?v=12.4.88';
+import { parseFile, parseBufferFiles, getAreaData, clearAreaData, generateKPIs, calculateBufferPallets, fetchBufferConfig, logSystemAction, pingServer, saveBufferReport, loadBufferReport, fetchBufferHistory, dataStore, setDateFilter, currentDateFilter, getUploadMeta, initPersistentData, updateTablaTallas } from '../services/csvHub_v6.js?v=12.4.90';
+import * as adminService from '../services/adminService.js?v=12.4.90';
 
 
-const VERSION = '12.4.89';
-const CACHE_KEY = `logistics_v12_4_89_`;
+const VERSION = '12.4.90';
+const CACHE_KEY = `logistics_v12_4_90_`;
 const DB_TASKS_KEY = 'almacenaje_tasks_history_v1';
 console.log(`[PULSE] Engine v${VERSION} Initialized (BETA)`);
 
