0daaecc4a3c2773fe87796e6f62094f9f2af1127
Admin <admin@local>
2026-05-08 19:03:51 -0500

    Automated task loading on tab entry in v12.4.85-BETA

--- beta/js/views/dashboard_v6.js
+++ beta/js/views/dashboard_v6.js
@@ -2,8 +2,8 @@ import { parseFile, parseBufferFiles, getAreaData, clearAreaData, generateKPIs, 
 import * as adminService from '../services/adminService.js?v=12.4.76';
 
 
-const VERSION = '12.4.84';
-const CACHE_KEY = `logistics_v12_4_84_`;
+const VERSION = '12.4.85';
+const CACHE_KEY = `logistics_v12_4_85_`;
 const DB_TASKS_KEY = 'almacenaje_tasks_history_v1';
 console.log(`[PULSE] Engine v${VERSION} Initialized (BETA)`);
 
@@ -2466,7 +2466,7 @@ export const renderDashboard = async (container, user, onLogout) => {
             renderUploadArea(wrap, 'articulos', dataStore.articulos, '.xlsx', 'MAESTRO ARTÍCULOS');
         }
     } else if (tabId === 'almacenaje' && activeSub === 'tareas_dia') {
-        renderAlmacenajeTareas(container);
+        loadAlmacenajeTasks().then(() => renderAlmacenajeTareas(container));
     } else {
         const data = await getAreaData(tabId);
         if (!data) renderUploadArea(container, tabId);
