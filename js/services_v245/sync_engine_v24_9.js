50bebb46eb434929fa7b56a0c668e2ce40facec7
Admin <admin@local>
2026-05-17 01:39:04 -0500

    chore: bump version to v25.1.44

--- js/services_v245/sync_engine_v24_9.js
+++ js/services_v245/sync_engine_v24_9.js
@@ -20,7 +20,7 @@ export const syncStore = {
 export let isFirstPullDone = false;
 
 export async function initSync() {
-    console.log("🚀 [PULSE] Inicializando Motor v25.1.43...");
+    console.log("🚀 [PULSE] Inicializando Motor v25.1.44...");
     try {
         await pullGlobal();
     } catch (e) {
